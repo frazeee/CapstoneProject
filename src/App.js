d3c4a6aaec4e5509f2a227a51b2472e950ee7eb6
Kyle Uy <kyleeeee09@gmail.com>
2023-09-19 15:36:44 +0800

    Added script imports & Cleaned React imports

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
-import Login from './pages/Login';
-import Register from './pages/Register'
-import LandingPage from './pages/landingPage';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Login, Register, LandingPage } from './pages' 
+import { BrowserRouter as Router, Routes, Route, json } from "react-router-dom";
+import { useEffect, useState } from 'react';
 
 
 
 function App() {
+
+  const [token, setToken] = useState(false)
+
+  if(token){
+    sessionStorage.setItem('token', JSON.stringify(token))
+
+  }
+
+  useEffect(()=>{
+    if(sessionStorage.getItem('token')){
+        let data =JSON.parse(sessionStorage.getItem('token'))
+        setToken(data)
+    }
+
+  }, [])
+
+
+
+
   return (
     <>
      <Router>
       <Routes>
-        <Route path ="/" element={<LandingPage />}/>
-        <Route path ="/Login" element={<Login />} />
+        <Route path ="/" element={<LandingPage token={token} />}/>
+        <Route path ="/Login" element={<Login setToken={setToken}/>} />
         <Route path='/Register' element={<Register />} />
       </Routes>
     </Router> 
